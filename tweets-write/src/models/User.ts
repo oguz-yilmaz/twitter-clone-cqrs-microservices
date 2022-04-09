0db66e88acac2d4afb60d90f289dfd24d8fdef74
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-04-09 17:23:45 +0300

    Adds User model to tweet-write

---
+++ tweets-write/src/models/User.ts
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
+
+interface UserAttributes {
+    id?: string
+    username: string
+    email: string
+    firstname: string
+    lastname: string
+}
+
+export interface UserDocument extends mongoose.Document {
+    id: string
+    username: string
+    email: string
+    firstname: string
+    lastname: string
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+    build(attrs: UserAttributes): UserDocument
+
+    findByVersion(event: { id: string; version: number }): Promise<UserDocument | null>
+}
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id
+
+                delete ret._id
+            },
+        },
+    }
+);
+
+userSchema.set('versionKey', 'version')
+userSchema.plugin(updateIfCurrentPlugin)
+
+userSchema.statics.build = (attrs: UserAttributes) => {
+
+    return new User({
+        _id: attrs.id,
+        username: attrs.username,
+        email: attrs.email,
+        firstname: attrs.firstname,
+        lastname: attrs.lastname,
+    })
+}
+
+userSchema.statics.findByVersion = (event: { id: string; version: number }) => {
+
+    return User.findOne({
+        _id: event.id,
+        version: event.version - 1
+    })
+}
+
+const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
+
+export { User };
